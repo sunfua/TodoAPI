64c4222209d9a2ca7a8fea771ae09c265671dd92
sunfua <sunfua@gmail.com>
2017-09-01 11:04:27 +0700

    Add CustomerControll

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TodoAPI.Controllers
 {
@@ -23,6 +24,18 @@ namespace TodoAPI.Controllers
         public IActionResult GetAll()
         {
             return new ObjectResult(_context.Customer.Where(c=>c.Poscode.StartsWith("65")).Take(10));
+        }
+        // GET: api/Customer/pageSize/pageNumber/orderBy(optional) 
+        [Route("{pageSize:int}/{pageNumber:int}/{orderBy:alpha?}")]
+        public async Task<IActionResult> Get(int pageSize, int pageNumber, string orderBy="")
+        {
+            var totalCount =_context.Customer.Count();
+            var totalPage = Math.Ceiling((double)totalCount/pageSize);
+            var sql="SELECT * FROM (SELECT Row_Number() OVER(ORDER BY CustomerCode) As RowID, * FROM Customer WHERE POSCode like '65%') As RowResults "+
+                    "WHERE RowID Between {0} AND {1}";            
+            var customer = await _context.Customer.FromSql(sql,(pageNumber-1)*pageSize + 1, pageNumber*pageSize).ToArrayAsync();
+            //var customer = await _context.Customer.Where(c=>c.Poscode.StartsWith("65")).OrderBy(c=>c.CustomerCode).Skip((pageNumber-1)*pageSize).Take(pageSize).ToListAsync();
+            return new ObjectResult(customer);
         }
     }
 }
